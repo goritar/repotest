a75d6ed6965a9ccc0f65ef9d58a70e3a8a18f811
goritar <goritar@gmail.com>
2016-07-22 15:32:13 +0300

    дз

--- src/tesGitHub.java
+++ src/tesGitHub.java
@@ -1,8 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Created by Игорь on 22.07.2016.
  */
 public class tesGitHub {
     public static void main(String[] args) {
-        System.out.println("Hello GitHub!");
+        System.out.print(" Введите длинну массива:");
+        Scanner sc = new Scanner(System.in);
+        int arrLength = sc.nextInt();
+        int[] arr = new int[arrLength];
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.println("введите "+ (i+1) + "й элемент массива:");
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort (Arrays.copyOf(arr,(int)(arr.length*1.5)));
+        System.out.println(Arrays.toString(arr));
+
     }
 }
+
